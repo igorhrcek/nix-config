50e90ac403980c8e4b133cd384ad46f6dd9e50b1
Igor Hrček <igor@hrcek.rs>
2025-07-20 21:03:11 +0200

    feat: fzf improvements

--- modules/shared/programs/fish/default.nix
+++ modules/shared/programs/fish/default.nix
@@ -31,6 +31,8 @@
       set -U --append __done_exclude '^vim'
       set -U --append __done_exclude '^nvim'
 
+      set -Ux FZF_DEFAULT_OPTS '--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4 --height 40% --layout reverse --border'
+
       nix-your-shell fish | source
     '';
 
