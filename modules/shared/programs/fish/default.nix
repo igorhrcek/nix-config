6b6173933df357bbcbbcebc244c61cc964c26742
Igor Hrček <igor@hrcek.rs>
2025-10-06 13:14:34 +0200

    feat: Change default editor

--- modules/shared/programs/fish/default.nix
+++ modules/shared/programs/fish/default.nix
@@ -19,7 +19,7 @@
       fish_add_path "$HOME/.krew/bin"
 
       set -gx SOPS_AGE_KEY_FILE $HOME/.config/sops/age/keys.txt
-      set -gx EDITOR code -w
+      set -gx EDITOR cursor -w
       set -gx LANG en_US.UTF-8
       set -gx LC_ALL en_US.UTF-8
       set -gx HOMEBREW_NO_ANALYTICS 1
